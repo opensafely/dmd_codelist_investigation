da9b2e8a06987841c1e541facb764b68e7c39449
Jon Massey <jon.massey@thedatalab.org>
2022-08-31 19:52:47 +0100

    all affected codelists in study

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,5 +1,14 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+from cohortextractor import StudyDefinition, patients
+from codelists import dict_codelists
 
+med_vars = {
+    k: patients.with_these_medications(
+        v,
+        between=["index_date", "index_date + 1 year"],
+        returning="number_of_matches_in_period",
+    )
+    for k, v in dict_codelists
+}
 
 study = StudyDefinition(
     default_expectations={
@@ -10,4 +19,5 @@ study = StudyDefinition(
     population=patients.registered_with_one_practice_between(
         "2019-02-01", "2020-02-01"
     ),
+    **med_vars
 )
